8a1b5281bdc49054dfd8802c9ee5774214b90839
uitaek <rlaejdwls@naver.com>
2018-11-07 18:44:47 +0900

    - 웹페이지 테스트

--- src/main/kotlin/com/example/demo/web/controller/HtmlController.kt
+++ src/main/kotlin/com/example/demo/web/controller/HtmlController.kt
@@ -1,13 +1,14 @@
 package com.example.demo.web.controller
 
 import com.example.demo.common.service.UserService
+import com.example.demo.common.util.Loggable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.*
 
-@Controller class HtmlController {
+@Controller class HtmlController: Loggable {
     @Autowired private lateinit var service: UserService
 
     @RequestMapping("/blog", method=[RequestMethod.GET])
@@ -17,5 +18,12 @@ import org.springframework.web.bind.annotation.*
         model["title"] = "Blog"
         model["users"] = service.selectUserForPagingUsingEntityManagerJpa(page, size)
         return "blog"
+    }
+    @RequestMapping("/log", method=[RequestMethod.GET])
+    fun log(model: Model): String {
+        logger.debug("debug log")
+        logger.info("info log")
+        model["title"] = "Main"
+        return "/index"
     }
 }
